c6eed014d6cba03bd0b7187b73aba80b0e73fb8d
tomprime <tom@primeacademy.io>
2020-04-03 16:33:06 -0500

    Added Supported component.

---
+++ src/components/Supported/Supported.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom';
+
+
+class Supported extends Component {
+//   state = {
+//     newElement: ''
+//   }
+
+//   handleElementChange = (event) => {
+//     this.setState({
+//       newElement: event.target.value,
+//     });
+//   }
+
+  render() {
+    return (
+      <div className="Supported">
+          <h2>How well are you being supported?</h2>
+        {/* <header className="App-header">
+          <h1 className="App-title">Feedback!</h1>
+          <h4><i>Don't forget it!</i></h4>
+        </header>
+        <br/>
+        <button onClick={() => this.props.dispatch({ type: 'BUTTON_ONE' })}>Button One</button>
+        <button onClick={() => this.props.dispatch({ type: 'BUTTON_TWO' })}>Button Two</button>
+        <br />
+        <label for="elementInput">Element</label>
+        <input type="text" placeholder="Enter Element Here" id="elementInput" onChange={this.handleElementChange} />
+        <button onClick={() => this.props.dispatch({ type: 'ADD_ELEMENT', payload: this.state.newElement})}>Add Element</button> */}
+      </div>
+    );
+  }
+}
+
+export default withRouter(Supported);
