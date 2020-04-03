19fe349429881554c87f8192c4a2a40758dd6875
tomprime <tom@primeacademy.io>
2020-04-03 16:31:41 -0500

    Added components (React Router Views).

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,25 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import axios from 'axios';
-import './App.css';
+import './App.css'; 
+import { HashRouter as Router, Route, Link } from "react-router-dom";
+import Feeling from '../Feeling/Feeling';
+import Understanding from '../Understanding/Understanding';
+import Supported from '../Supported/Supported';
+import Comments from '../Comments/Comments';
+import Review from '../Review/Review';
 
 
 class App extends Component {
+  state = {
+    newElement: ''
+  }
+
+  handleElementChange = (event) => {
+    this.setState({
+      newElement: event.target.value,
+    });
+  }
+
   render() {
     return (
       <div className="App">
@@ -13,10 +28,23 @@ class App extends Component {
           <h4><i>Don't forget it!</i></h4>
         </header>
         <br/>
+
+      <Router>
+        <Route path="/feeling" component={Feeling} />
+        <Route path="/supported" component={Supported} />   
+        <Route path="/comments" component={Comments} />
+        <Route path="/review" component={Review} /> 
+      </Router>
+        
+
+
         {/* Dispatching an action */}
-        <button onClick={() => this.props.dispatch({ type: 'BUTTON_ONE' })}>Button One</button>
+        {/* <button onClick={() => this.props.dispatch({ type: 'BUTTON_ONE' })}>Button One</button>
         <button onClick={() => this.props.dispatch({ type: 'BUTTON_TWO' })}>Button Two</button>
-        <button onClick={() => this.props.dispatch({ type: 'ADD_ELEMENT', payload: 'hydrogen'})}>Add Element</button>
+        <br />
+        <label for="elementInput">Element</label>
+        <input type="text" placeholder="Enter Element Here" id="elementInput" onChange={this.handleElementChange} />
+        <button onClick={() => this.props.dispatch({ type: 'ADD_ELEMENT', payload: this.state.newElement})}>Add Element</button> */}
       </div>
     );
   }
