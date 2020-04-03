2feda2fbf179dbd7a7d8bd27083f18f2d21b42be
tomprime <tom@primeacademy.io>
2020-04-03 17:22:40 -0500

    Added Home component as a starter to add new feedb

---
+++ src/components/Home/Home.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom';
+
+class Home extends Component {
+
+    handleClick = () => {
+        this.props.history.push('/feeling')
+      } 
+
+    render() {
+        return (
+            <button onClick={this.handleClick}>Add New Feedback</button>
+        )
+    }
+}
+
+export default withRouter(Home);
