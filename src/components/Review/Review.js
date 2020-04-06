ec9194c1f00bcae588a4229896cb4be51348c524
tomprime <tom@primeacademy.io>
2020-04-06 17:27:21 -0500

    Added TODO notes

--- src/components/Review/Review.js
+++ src/components/Review/Review.js
@@ -3,21 +3,24 @@ import { withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 const putReduxStateOnProps = (reduxState) => ({
-    reduxState
-  });
+  reduxState
+});
 
 class Review extends Component {
 
   saveFeedback(event) {
     console.log('In saveFeedback()');
-    //TODO axios put
+    //TODO axios PUSH of new feedback 
+
+    //TODO then, show "Thank You" msg, and button to add more feedback
+    //maybe reuse Home view for this? 
   }
 
   render() {
     return (
       <div className="Review">
-          <h2>Review Your Feedback</h2>
-    <p>Feeling: {this.props.reduxState.feelingReducer.feelingRating}</p>
+        <h2>Review Your Feedback</h2>
+        <p>Feeling: {this.props.reduxState.feelingReducer.feelingRating}</p>
         <p>Understanding: {this.props.reduxState.understandingReducer.understandingRating}</p>
         <p>Support: {this.props.reduxState.supportedReducer.supportedRating}</p>
         <p>Comments: {this.props.reduxState.commentsReducer.comments}</p>
