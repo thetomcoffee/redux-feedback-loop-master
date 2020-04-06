82debb5695f78fec4547709ea884837bca02bfab
tomprime <tom@primeacademy.io>
2020-04-06 17:12:21 -0500

    Displaying feedback info on the DOM, stubbed out saveFeedback() function.

--- src/components/Review/Review.js
+++ src/components/Review/Review.js
@@ -8,11 +8,20 @@ const putReduxStateOnProps = (reduxState) => ({
 
 class Review extends Component {
 
+  saveFeedback(event) {
+    console.log('In saveFeedback()');
+    //TODO axios put
+  }
+
   render() {
     return (
       <div className="Review">
           <h2>Review Your Feedback</h2>
-        <pre>{JSON.stringify(this.props.reduxState)}</pre>
+    <p>Feeling: {this.props.reduxState.feelingReducer.feelingRating}</p>
+        <p>Understanding: {this.props.reduxState.understandingReducer.understandingRating}</p>
+        <p>Support: {this.props.reduxState.supportedReducer.supportedRating}</p>
+        <p>Comments: {this.props.reduxState.commentsReducer.comments}</p>
+        <input type="submit" onClick={this.saveFeedback}></input>
       </div>
     );
   }
