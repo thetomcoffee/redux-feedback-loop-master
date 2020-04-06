68a1db09bca8f0301056a2a0ca519825bbe9c4d5
tomprime <tom@primeacademy.io>
2020-04-06 11:08:22 -0500

    Added code to show Redux state.

--- src/components/Review/Review.js
+++ src/components/Review/Review.js
@@ -1,36 +1,21 @@
 import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom';
+import { connect } from 'react-redux';
 
+const putReduxStateOnProps = (reduxState) => ({
+    reduxState
+  });
 
 class Review extends Component {
-//   state = {
-//     newElement: ''
-//   }
-
-//   handleElementChange = (event) => {
-//     this.setState({
-//       newElement: event.target.value,
-//     });
-//   }
 
   render() {
     return (
       <div className="Review">
           <h2>Review Your Feedback</h2>
-        {/* <header className="App-header">
-          <h1 className="App-title">Feedback!</h1>
-          <h4><i>Don't forget it!</i></h4>
-        </header>
-        <br/>
-        <button onClick={() => this.props.dispatch({ type: 'BUTTON_ONE' })}>Button One</button>
-        <button onClick={() => this.props.dispatch({ type: 'BUTTON_TWO' })}>Button Two</button>
-        <br />
-        <label for="elementInput">Element</label>
-        <input type="text" placeholder="Enter Element Here" id="elementInput" onChange={this.handleElementChange} />
-        <button onClick={() => this.props.dispatch({ type: 'ADD_ELEMENT', payload: this.state.newElement})}>Add Element</button> */}
+        <pre>{JSON.stringify(this.props.reduxState)}</pre>
       </div>
     );
   }
 }
 
-export default withRouter(Review);
+export default connect(putReduxStateOnProps)(withRouter(Review));
