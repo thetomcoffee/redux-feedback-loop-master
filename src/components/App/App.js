2e895e2e1b65afa4874962c9b3c117d3ef9f23b7
tomprime <tom@primeacademy.io>
2020-04-06 09:18:02 -0500

    Warning "Link is defined but never used" fixed by removing Link in import from react-router-dom.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import './App.css'; 
-import { HashRouter as Router, Route, Link } from "react-router-dom";
+import { HashRouter as Router, Route } from "react-router-dom";
 import Feeling from '../Feeling/Feeling';
 import Understanding from '../Understanding/Understanding';
 import Supported from '../Supported/Supported';
